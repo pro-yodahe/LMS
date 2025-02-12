11c1cd474e9c5fcccb7aa43fc7c57883b003ba60
pro-yodahe <yodahejob@gmail.com>
2025-02-12 22:08:04 +0300

    Added chapter reorder functionality using drag and drop

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapter-from.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapter-from.tsx
@@ -4,7 +4,7 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, PlusCircle } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
@@ -21,6 +21,7 @@ import {
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { ChaptersList } from "./chapters-list";
 
 interface ChaptersFormProps {
     initialData: Course & { chapters: Chapter[] };
@@ -36,10 +37,10 @@ export const ChaptersForm = ({
     courseId,
 }: ChaptersFormProps) => {
     const [isCreating, setIsCreating] = useState(false);
-    const [isEditing, setIsEditing] = useState(false);
+    const [isUpdating, setIsUpdating] = useState(false);
     const router = useRouter();
 
-    const toggleEdit = () => setIsEditing((current) => !current);
+    const toggleEdit = () => setIsUpdating((current) => !current);
     const toggleCreating = () => setIsCreating((current) => !current);
 
     const form = useForm<z.infer<typeof formSchema>>({
@@ -60,8 +61,34 @@ export const ChaptersForm = ({
         }
     };
 
+    const onReorder = async (updateData: {id: string, position: number}[]
+        ) => {
+            try {
+                setIsUpdating(true);
+
+                await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+                  list: updateData,  
+                })
+                toast.success("Chapters reordered");
+                router.refresh();
+            } catch (error) {
+                toast.error("Something went wrong");
+            } finally {
+                setIsUpdating(false);
+            }
+        }
+
+        const onEdit = (id: string) => {
+            router.push(`/teacher/courses/${courseId}/chapters/${id}`);
+        }
+
     return (
-        <div className="mt-6 border bg-slate-100 rounded-md p-4">
+        <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+            {isUpdating && (
+                <div className="absolute h-full w-full bg-slate-500/50 top-0 right-0 flex items-center justify-center">
+                    <Loader2 className="anmate-spin h-6 w-6 text-sky-700"/>
+                </div>
+            )}
             <div className="font-medium flex items-center justify-between">
                 Course chapters
                 <Button onClick={toggleCreating} variant="ghost">
@@ -114,7 +141,11 @@ export const ChaptersForm = ({
                 !initialData.chapters.length && "text-slate-500 italic"
                 )}>
                    {!initialData.chapters.length && "No chapters"}
-                   {/* TODO: Add a list of chapters*/}
+                   <ChaptersList
+                    onEdit={onEdit}
+                    onReorder={onReorder}
+                    items={initialData.chapters || []}
+                   />
                 </div>
                 
             )}
